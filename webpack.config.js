f39d6fde8669a4b26fe706aae73a0ddbdf8596d6
PhilippMatthes <github@philippmatth.es>
2020-11-12 16:58:00 +0100

    Add SASS build to Webpack to deliver stylesheets

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 const path = require('path');
 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   mode: 'production',
 
   devtool: 'source-map',
 
   entry: {
-    'index': [
+    'js/header.min.js':
       path.resolve(__dirname, 'homepage/static/src/ts/header.ts'),
+    'js/apps.min.js':
       path.resolve(__dirname, 'homepage/static/src/ts/apps.ts'),
-    ],
+    'css/homepage.min.js':
+      path.resolve(__dirname, 'homepage/static/src/css/homepage.js'),
+    'css/index.min.js':
+      path.resolve(__dirname, 'homepage/static/src/css/index.js'),
+    'css/navigation.min.js':
+      path.resolve(__dirname, 'homepage/static/src/css/navigation.js'),
+    'css/opensans.min.js':
+      path.resolve(__dirname, 'homepage/static/src/css/opensans.js'),
   },
 
   output: {
-    path: path.resolve(__dirname, 'homepage/static/dist/js'),
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, 'homepage/static/dist/'),
+    filename: '[name]'
   },
 
   resolve: {
@@ -31,6 +41,26 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+          'sass-loader',
+        ],
+      },
     ],
   },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
 };
